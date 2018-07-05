3158865fa090d472755fb725029ee6a4c59d99c7
Morpheusthewhite <zffromGerace@hotmail.it>
2018-07-05 20:52:30 +0200

    Fixed forgotten code upload

--- subman.py
+++ subman.py
@@ -1,44 +1,127 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
 
-from sys import argv
-from settingUtil import *
-import Util
+###---GUI version of subman---###
 
-#TODO: use argparse
-#TODO: give the user the possibility to play last video
-
-if len(argv) > 1 and argv[1] == "-h" :
-    print("Script for launching videos with subs in specified folders. Reminds of the last episode watched")
-    print("Configure usage: subman PATH_TO_VIDEOS PATH_TO_SUBS")
-    print("Next usages: subman")
-    print("If called with two arguments sets it as new video and sub folder")
-    print("The specified folders must contains only videos and subs (they cannot be in the same)")
-    exit()
-elif len(argv) > 2:
-    print("Specified some path, creating setting file...")
-    createSettingAndCountFile(argv[1], argv[2])
-elif not existSettingAndCountFile():
-    print("Setting file does not exist and too few arguments")
-    print("Errore: paths unspecified. -h for help")
-    exit()
-
+from tkinter import *
+from tkinter import filedialog, messagebox
+import os, bin.settingUtil, bin.playUtil, bin.dataStorage
 import subprocess
 
-(count, pathToVideo, pathToSubs, videoPattern, subsPattern) = readSetting()
+CURRENT_PATH = os.getcwd()
 
-print("Calculating next video and next subtitles file")
-(nextVid, nextSub) = Util.getNext(pathToVideo, videoPattern, pathToSubs, subsPattern, count)
+class gui(Tk):
+    def __init__(self):
+        super(gui, self).__init__()
+        self.wm_title("subman")
 
-if nextVid is None and nextSub is None:
-    print("No more files are available, exiting...")
-    exit(1)
+        #set the icon
+        self.__imgicon__ = PhotoImage(file=os.path.join(CURRENT_PATH, "media", 'iconWhite.png'))
+        self.tk.call('wm', 'iconphoto', self._w, self.__imgicon__)
 
-print("Launching vlc...")
-vlc_args = ["vlc", "--fullscreen", "--sub-file", nextSub, "--play-and-exit", nextVid]
-vlc = subprocess.Popen(vlc_args)
-vlc.wait()
+        self.frameVideo = Frame(self, borderwidth=1, relief="groove")
+        self.frameVideo.videoLabel = Label(self.frameVideo, text="Video")
+        self.frameVideo.videoLabel.pack(side="left")
+        self.frameVideo.videoPathText = Entry(self.frameVideo, width=30)
+        self.frameVideo.videoPathText.pack(side="left", padx=4, pady=7, fill="x", expand=True)
+        self.frameVideo.videoOpenFolder = Button(self.frameVideo, text="...", height=1, width=1, command=self.chooseVid)
+        self.frameVideo.videoOpenFolder.pack(side="right")
+        self.frameVideo.pack(pady=5, fill="x", expand=True)
 
-#TODO: manage real end of the video and end of the application
+        """ WIP
+        self.checkbutton = Checkbutton(self, text="Subtitled")
+        self.checkbutton.pack(pady=5)
+        """
 
-print("Updating count file...")
-updateCountFile(count)
+        self.frameSubs = Frame(self, borderwidth=1, relief="groove")
+        self.frameSubs.subsLabel = Label(self.frameSubs, text="Subs")
+        self.frameSubs.subsLabel.pack(side="left")
+        self.frameSubs.subsPathText = Entry(self.frameSubs, width=30)
+        self.frameSubs.subsPathText.pack(side="left", padx=4, pady=7, fill="x", expand=True)
+        self.frameSubs.subsOpenFolder = Button(self.frameSubs, text="...", height=1, width=1, command=self.chooseSubs)
+        self.frameSubs.subsOpenFolder.pack(side="right")
+        self.frameSubs.pack(pady=5, fill="x", expand=True)
+
+        self.buttonsFrame = Frame(self)
+        self.buttonsFrame.applyButton = Button(self, text="Apply", command=self.apply)
+        self.buttonsFrame.applyButton.pack(side="left")
+        self.buttonsFrame.playButton = Button(self, text="Play", command=self.play)
+        self.__buttonImage__ = PhotoImage(file=os.path.join(CURRENT_PATH, "media", 'playButtonGreen.png'))
+        self.buttonsFrame.playButton.config(image=self.__buttonImage__, height=30, width=30)
+        self.buttonsFrame.playButton.pack(side="left", padx=120, expand=True)
+        self.buttonsFrame.applyButton = Button(self, text="Reset", command=self.reset)
+        self.buttonsFrame.applyButton.pack(side="left")
+
+        self.center_window(400, 160)
+
+        if bin.settingUtil.existSettingAndCountFile():
+            # reading saved datas
+            (count, videoPath, subsPath, videoPattern, subsPattern) = bin.settingUtil.readSetting()
+            self.playInfo = bin.dataStorage.DataStorage(count, videoPath, videoPattern, subsPath, subsPattern)
+
+            # setting text areas with saved text
+            self.frameVideo.videoPathText.insert(END, self.playInfo.videoPath)
+            self.frameSubs.subsPathText.insert(END, self.playInfo.subsPath)
+
+    def updateStorage(self, ):
+        (count, videoPath, subsPath, videoPattern, subsPattern) = bin.settingUtil.readSetting()
+        self.playInfo = bin.dataStorage.DataStorage(count, videoPath, videoPattern, subsPath, subsPattern)
+
+    def play(self):
+        (nextVideo, nextSub) = bin.playUtil.getNext(self.playInfo.videoPath, self.playInfo.videoPattern, self.playInfo.subsPath,
+                             self.playInfo.subsPattern, self.playInfo.count)
+
+        if nextVideo is None and nextSub is None:
+            messagebox.showwarning("Error", "No more video are available in the specified directory")
+
+        vlc_args = ["vlc", "--fullscreen", "--sub-file", nextSub, "--play-and-exit", nextVideo]
+        vlc = subprocess.Popen(vlc_args)
+        vlc.wait()
+
+        bin.settingUtil.updateCountFile(self.playInfo.count)
+        self.playInfo.count += 1
+
+    def apply(self):
+        if bin.settingUtil.existSettingAndCountFile():
+            response = messagebox.askokcancel(title="Confirm", message="This will replace previous configuration file. Are you sure?")
+            if response:
+                bin.settingUtil.createSettingAndCountFile(self.frameVideo.videoPathText.get(), self.frameSubs.subsPathText.get())
+
+
+    def reset(self):
+        if bin.settingUtil.existSettingAndCountFile():
+            response = messagebox.askokcancel(title="Confirm",
+                                              message="This will remove previous configuration file. Are you sure?")
+            if response:
+                self.frameSubs.subsPathText.delete(0, 'end')  # clear text
+                self.frameVideo.videoPathText.delete(0, 'end')  # clear text
+                os.remove(os.getcwd()+"/count.txt")
+                os.remove(os.getcwd()+"/settings.ini")
+
+
+
+    def chooseSubs(self):
+        text = filedialog.askdirectory(title="Select subs folder")
+        if not text == "":
+            self.frameSubs.subsPathText.delete(0, 'end')  # clear text
+            self.frameSubs.subsPathText.insert(END, text)
+
+
+    def chooseVid(self):
+        text = filedialog.askdirectory(title="Select subs folder")
+        if not text == "":
+            self.frameVideo.videoPathText.delete(0, 'end')  # clear text
+            self.frameVideo.videoPathText.insert(END, text)
+
+    def center_window(self, width=300, height=200):
+        # get screen width and height
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        # calculate position x and y coordinates
+        x = (screen_width / 2) - (width / 2)
+        y = (screen_height / 2) - (height / 2)
+        self.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+
+mainWindow = gui()
+mainWindow.mainloop()
