627d006cafa8401d86502f35f8fe9700a5f3b914
morpheusthewhite <zffromGerace@hotmail.it>
2019-01-07 21:25:14 +0100

    corrected installation script dependencies and interpreter

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ if ! [ -z `which apt-get 2> /dev/null` ]; # Debian
     then sudo apt-get install vlc python python-tkinter 2> /dev/null
 fi
 if ! [ -z `which dnf 2> /dev/null` ]; # Fedora
-    then sudo dnf install vlc python python-tkinter 2> /dev/null
+    then sudo dnf install vlc python3 python3-tkinter 2> /dev/null
 fi
 if ! [ -z `which pacman 2> /dev/null` ]; # Arch Linux
-    then sudo pacman install vlc python python-tkinter 2> /dev/null
+    then sudo pacman install vlc python3 python3-tkinter 2> /dev/null
 fi
 
 current_dir=`pwd`
@@ -21,7 +21,7 @@ Version=1.0
 Name=subman
 Comment=A subtitle manager tool
 Path="$current_dir"
-Exec=python3.6 "$current_dir"/subman.py
+Exec=python3 "$current_dir"/subman.py
 Icon="$current_dir"/media/iconWhite.png
 Terminal=false
 Categories=Media;" | sudo tee /usr/local/share/applications/subman.desktop > /dev/null
